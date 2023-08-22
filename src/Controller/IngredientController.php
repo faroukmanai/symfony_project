d0e72fc04f039747a0894e41ceab93f6c6deb8bd
faroukmanai <79013591+faroukmanai@users.noreply.github.com>
2023-08-22 14:25:28 -0400

    form validation add ingredient | flash messages

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -1,8 +1,12 @@
 <?php
 
 namespace App\Controller;
+
+use App\Entity\Ingredient;
+use App\Form\IngredientType;
 use Knp\Component\Pager\PaginatorInterface;
 use App\Repository\IngredientRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -29,6 +33,35 @@ class IngredientController extends AbstractController
     );
         return $this->render('pages/ingredient/index.html.twig', [
             'ingredients' => $ingredients
+        ]);
+    }
+
+    #[Route('/ingredient/nouveau', 'ingredient.new', methods:['GET','POST'])]
+    public function new(
+        Request $request,
+        EntityManagerInterface $manager
+        ) : Response {
+
+        $ingredient = new Ingredient();
+        $form = $this->createForm(IngredientType::class, $ingredient);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ingredient = $form->getData();
+
+            $manager->persist($ingredient);
+            $manager->flush();
+
+        
+            $this->addFlash(
+                'success',
+                'Votre ingrédient a été crée avec success'
+            );
+            return $this->redirectToRoute('app_ingredient');
+        }
+
+        return $this->render('pages/ingredient/new.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
