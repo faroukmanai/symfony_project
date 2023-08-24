d34778f3f5271afa385f526711af433148eb159b
faroukmanai <79013591+faroukmanai@users.noreply.github.com>
2023-08-24 13:05:28 -0400

    create form in recipetype and twig

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,25 +2,67 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Recipe;
+use App\Form\RecipeType;
+use App\Repository\RecipeRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Knp\Component\Pager\PaginatorInterface;
-use App\Repository\IngredientRepository;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class RecipeController extends AbstractController
 {
+    /**
+     * display all recipes
+     *
+     * @param RecipeRepository $repository
+     * @param PaginatorInterface $paginator
+     * @param Request $request
+     * @return Response
+     */
+
     #[Route('/recette', name: 'recipe.index', methods: ['GET'])]
-    public function index(IngredientRepository $repository, PaginatorInterface $paginator, Request $request): Response
+    public function index(RecipeRepository $repository, PaginatorInterface $paginator, Request $request): Response
     {
-        $ingredients = $paginator->paginate(
+            $recipes = $paginator->paginate(
             $repository->findAll() , 
             $request->query->getInt('page', 1), 
             10 
             );
+
         return $this->render('pages/recipe/index.html.twig', [
-            'controller_name' => 'RecipeController',
+            'recipes' => $recipes
         ]);
+    }
+
+    #[Route('/recette/creation', name: 'recipe.new', methods: ['GET', 'POST'])]
+    public function new(
+        Request $request,
+        EntityManagerInterface $manager) 
+        : Response
+    {
+        $recipe = new Recipe();
+        $form = $this->createForm(RecipeType::class, $recipe);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ingredient = $form->getData();
+
+            $manager->persist($ingredient);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Votre recette a été crée avec succès'
+            );
+            return $this->redirectToRoute('recipe.index');
+        }
+
+        return $this->render('pages/recipe/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+         
     }
 }
